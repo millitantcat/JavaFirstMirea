0cfa57efe13c35ce3ca8f860338fe2435a6c6ddf
millitantcat <millitantcat@github.com>
2021-10-28 12:54:52 +0300

    живое....почти не мёртвое

---
+++ src/ru/mirea/task16/Main.java
@@ -0,0 +1,10 @@
+package ru.mirea.task16;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame login = new loginForm();
+        login.setVisible(true);
+    }
+}
